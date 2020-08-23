3042f441d375ffd115ee5b528f90ee256df52a15
Rishabh Roshan Mallik <rishabhsun.71199@gmail.com>
2020-08-23 20:54:17 +0530

    Updates

---
+++ precache-manifest.e7768671a294489f6e62661ca03fde29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be302290ff291767e0de7e9cc15fed39",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "ba773021eeb682a4403c",
+    "url": "/calculator/static/css/main.e604ac72.chunk.css"
+  },
+  {
+    "revision": "ecada51e1a3334c42b99",
+    "url": "/calculator/static/js/2.e6f076d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.e6f076d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba773021eeb682a4403c",
+    "url": "/calculator/static/js/main.741f097b.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
